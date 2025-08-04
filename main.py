b3c8962cbfa59b4e741b372f6f1c32e673c34c12
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2025-08-04 17:08:07 -0500

    Initial commit.

---
+++ main.py
@@ -0,0 +1,21 @@
+# PyTorch Test
+# Started on 2025/08/04
+# by Drew Wingfield
+
+#region Imports
+#endregion Imports
+
+#region Setup
+#endregion Setup
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    print("Running as main.")
+    main()
+
+
+
+# -- End of file --
