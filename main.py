450dfaf6d8f54b8f3a8235013b49dd2815fd3618
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2025-08-04 17:41:55 -0500

    WIP

--- main.py
+++ main.py
@@ -4,6 +4,14 @@
 
 #region Imports
 import logging
+
+import torch # I used "python -m pip install -r .\requirements.txt --index-url https://download.pytorch.org/whl/cu128" - see https://pytorch.org/get-started/locally/ for details.
+from torch import nn
+from torch.utils.data import DataLoader
+
+import pandas as pd
+import numpy as np
+
 #endregion Imports
 
 #region Setup
@@ -11,10 +19,15 @@ logger = logging.getLogger(__name__)
 logging.basicConfig()
 logger.setLevel(logging.DEBUG)
 logger.debug("Logging set up correctly.")
+
+logger.info(f"CUDA: {torch.cuda.is_available()}")
+logger.info(f"Found {torch.accelerator.device_count()} accelerator devices.")
+
 #endregion Setup
 
 def main() -> None:
-    pass
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    logger.info(f"Using {device} device")
 
 
 if __name__ == "__main__":
